7fa3f32a029ba61f34e788f474310ab35d923c50
Kang0325 <noworry1@naver.com>
2021-06-24 15:16:36 +0900

    설정화면 개발 완료

--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -9,6 +10,34 @@ namespace MRPApp.Logic
 {
     public class DataAccess
     {
-        
+        // 세팅테이블에서 데이터 가져오기
+        internal static List<Settings> GetSettings()
+        {
+            List<Model.Settings> settings;
+
+            using (var ctx = new MRPEntities())
+                settings = ctx.Settings.ToList();
+
+            return settings;
+        }
+
+        internal static int SetSettings(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                ctx.Settings.AddOrUpdate(item);
+                return ctx.SaveChanges(); // Commit
+            }
+        }
+
+        internal static int DelSettings(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                var obj = ctx.Settings.Find(item.BasicCode); // 검색한 실제 데이터를 삭제
+                ctx.Settings.Remove(obj); // Delete
+                return ctx.SaveChanges(); // Commit
+            }
+        }
     }
 }
